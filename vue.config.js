330138608b7938cd24afe3229fa49e31f226cdba
Jonatas Martins <jontas.martins@seplag.ce.gov.br>
2021-11-07 21:04:43 -0300

    Removing source map on production

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   publicPath: "./",
   outputDir: "docs",
+  productionSourceMap: false,
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       args[0].title = "Text Formatting";
